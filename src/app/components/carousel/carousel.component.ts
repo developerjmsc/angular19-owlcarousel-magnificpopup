df1e8998b2639670cdb1cb98d7163ef5233868ca
developerjmsc <developer.jmsc@gmail.com>
2025-02-02 16:59:15 -0400

    magnific popup

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { PicsumService } from '../../services/picsum.service';
 import { Album } from '../../interfaces/album';
+import JQuery from 'jquery';
 
 @Component({
   selector: 'component-carousel',
   imports: [CarouselModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
 
@@ -36,14 +37,39 @@ export class CarouselComponent implements OnInit {
     },
   }
 
+  constructor() {
+    effect(() => this.initializeMagnificPopup());
+  }
+
   ngOnInit(): void {
     this._picsumService.get().subscribe({
       next: (response: Album[]) => {
-        console.log(response);
-        
         this._albums.set(response);
+        this.initializeMagnificPopup();
       }
     });
   }
+
+  initializeMagnificPopup() {
+    ($('#listcarousel') as any).magnificPopup({
+      delegate: '.owl-item:not(.cloned) .showfullimg',
+      type: 'image',
+      removalDelay: 300,
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1],
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function (item: any) {
+          return item.el.attr('title');
+        },
+      }
+    });
+  }
+
 
 }
